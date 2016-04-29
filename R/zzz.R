5511f66e6b6aa201e17929fe4167b607b255099b
wwwjk366 <tanbingy@gmail.com>
2016-04-29 14:01:08 -0400

    addd

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Compass Digital Labs Internal Use only.")
+  packageStartupMessage("\nThis package is for Compass Digital Labs Internal Use only. \n Do not use for other purposes!")
 }
